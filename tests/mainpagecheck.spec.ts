aa063d5a101efac930ff1314fa853d6c81101625
Nazarii Melnychuk <nazarii.melnychuk@dataart.com>
2026-01-20 21:59:45 +0000

    переробка тесту

--- tests/mainpagecheck.spec.ts
+++ tests/mainpagecheck.spec.ts
@@ -7,20 +7,36 @@ test.describe ('перевірка головної сторінки',() => {
         email: `user@demo.com`,
         password: 'user123',
         };
+        //перевірка наявності необхідниї полів//
         const enterEmail=page.getByPlaceholder('your@email.com');
         await expect(enterEmail).toBeVisible();
-        await (enterEmail).fill(user.email);
-        await expect(enterEmail).toHaveValue ('user@demo.com');
         const enterPassword=page.getByPlaceholder('Введіть пароль');
         await expect(enterPassword).toBeVisible();
-        await (enterPassword).fill(user.password);
-        await expect(enterPassword).toHaveValue ('user123');
         const enterButton=page.getByTestId('login-submit-button');
         await expect(enterButton).toHaveText('Увійти');
         await expect(enterButton).toBeEnabled();
+        //заповнення реєстраційної форм//
+        await test.step('Заповнення реєстраційної форми', async () => {
+        await enterEmail.fill(user.email);
+        await enterPassword.fill(user.password);
+        await expect(enterEmail).toHaveValue ('user@demo.com');
+        await expect(enterPassword).toHaveValue ('user123');
+        });
+        //завантаження головної сторінки//
         await Promise.all([page.waitForURL('https://finmore.netlify.app'),enterButton.click(),]);
         await expect(page).toHaveTitle('Повнофункціональний фінансовий менеджер');
-        
+        //перевірка головної сторінки//
+        const userlogo=page.locator('text=User Demo');
+        await expect(userlogo).toBeEnabled();
+        await expect(userlogo).toHaveText('User Demo');
+        const mainlogo=page.locator('text=FinanceManager');
+        await expect(mainlogo).toBeEnabled();
+        await expect(mainlogo).toHaveText('FinanceManager');
+        //на сторінці два елементи з data-testid="app-title"//
+        const secondMainlogo = page.getByTestId('sidebar').getByTestId('app-title');
+        await expect(secondMainlogo).toBeVisible();
+        await expect(secondMainlogo).toHaveText('FinanceManager');
+
 
 
       
